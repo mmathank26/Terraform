673beb042468df3400c07f14b7bcb5e1bc793c10
mmathank26 <meghalmathankar@gmail.com>
2025-06-16 14:43:32 +0530

    updated the VPC - variable and output file

---
+++ AWS_VPC/variable.tf
@@ -0,0 +1,25 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "136.10.0.0/16"
+}
+
+variable "cluster_name" {
+  description = "Name of the cluster"
+  type        = string
+  default     = "my-cluster"    
+  
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["136.10.1.0/24", "136.10.2.0/24"]
+
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["136.10.3.0/24", "136.10.4.0/24"]
+}
